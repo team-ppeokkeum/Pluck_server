f0589235a17b1c69345baff8814b1f552e978d0e
dkflfkd53 <b57062562@gmail.com>
2024-03-20 17:05:15 +0900

    🌧 :: findByAccountId 메서드 추가

--- src/main/java/com/example/pluck_server/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/pluck_server/domain/user/dao/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.pluck_server.domain.user.dao;
 import com.example.pluck_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
-    
+    Optional<User> findByAccountId(String accountId);
 }
