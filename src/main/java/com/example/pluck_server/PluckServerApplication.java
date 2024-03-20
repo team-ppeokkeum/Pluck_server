330badeedebf7ddf37f34da9290483df35963cec
dkflfkd53 <b57062562@gmail.com>
2024-03-20 11:35:48 +0900

    first commit

---
+++ src/main/java/com/example/pluck_server/PluckServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pluck_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluckServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PluckServerApplication.class, args);
+    }
+
+}
