27f05c6e3ef5f8b63fc36dd4e9b8d24c738bfcc9
dkflfkd53 <b57062562@gmail.com>
2024-03-23 00:43:05 +0900

    🌧 :: refreshTokenRepository 추가

---
+++ src/main/java/com/example/pluck_server/domain/auth/dao/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.pluck_server.domain.auth.dao;
+
+import com.example.pluck_server.domain.auth.domain.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
+}
