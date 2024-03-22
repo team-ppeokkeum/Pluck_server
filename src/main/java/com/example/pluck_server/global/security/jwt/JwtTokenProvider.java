67cd78176ca7e11cdc91ddf413314d5c23429c82
dkflfkd53 <b57062562@gmail.com>
2024-03-23 00:59:01 +0900

    🌧 :: JwtTokenProvider 추가

---
+++ src/main/java/com/example/pluck_server/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,120 @@
+package com.example.pluck_server.global.security.jwt;
+
+import com.example.pluck_server.domain.auth.dao.RefreshTokenRepository;
+import com.example.pluck_server.domain.auth.domain.RefreshToken;
+import com.example.pluck_server.domain.auth.dto.response.LoginResponse;
+import com.example.pluck_server.domain.auth.exception.ExpiredTokenException;
+import com.example.pluck_server.domain.auth.exception.InvalidTokenException;
+import com.example.pluck_server.domain.user.dao.UserRepository;
+import com.example.pluck_server.domain.user.domain.User;
+import com.example.pluck_server.domain.user.exception.UserNotFoundException;
+import com.example.pluck_server.global.security.auth.CustomUserDetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+
+    private final UserRepository userRepository;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    // access token 생성
+    public String createAccessToken(String accountId) {
+
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setSubject(accountId)
+                .claim("type", "access")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getAccessExpiration() * 1000))
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+                .compact();
+    }
+
+    public String createRefreshToken(String accountId) {
+
+        Date now = new Date();
+
+        String refreshToken = Jwts.builder()
+                .claim("type", "refresh")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getRefreshExpiration() * 1000))
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+                .compact();
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .accountId(accountId)
+                        .token(refreshToken)
+                        .timeToLive(jwtProperties.getRefreshExpiration())
+                        .build());
+
+        return refreshToken;
+    }
+
+    // 토큰에 담겨있는 userId로 SpringSecurity Authentication 정보를 반환하는 메서드
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts
+                    .parser()
+                    .setSigningKey(jwtProperties.getSecret())
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw ExpiredTokenException.EXCEPTION;
+        } catch (Exception e) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
+
+    public LoginResponse receiveToken(String accountId) {
+
+        Date  now = new Date();
+
+        User user = userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        return LoginResponse
+                .builder()
+                .accessToken(createAccessToken(accountId))
+                .refreshToken(createRefreshToken(accountId))
+                .accessExpiredAt(new Date(now.getTime() + jwtProperties.getAccessExpiration()))
+                .refreshExpiredAt(new Date(now.getTime() + jwtProperties.getRefreshExpiration()))
+                .build();
+    }
+
+    // HTTP 요청 헤더에서 토큰을 가져오는 메서드
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(jwtProperties.getHeader());
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(jwtProperties.getPrefix())
+                && bearerToken.length() > jwtProperties.getPrefix().length() + 1) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
