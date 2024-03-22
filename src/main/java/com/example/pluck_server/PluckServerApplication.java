f992587c774ff5de43731fe0ca795aac558a8c91
dkflfkd53 <b57062562@gmail.com>
2024-03-23 01:00:11 +0900

    🌧 :: ConfigurationScan 어노테이션 추가

--- src/main/java/com/example/pluck_server/PluckServerApplication.java
+++ src/main/java/com/example/pluck_server/PluckServerApplication.java
@@ -2,8 +2,10 @@ package com.example.pluck_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PluckServerApplication {
 
     public static void main(String[] args) {
