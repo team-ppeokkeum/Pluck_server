25c1b6f6d557589af4fdd634c958ac67fd4c110c
dkflfkd53 <b57062562@gmail.com>
2024-03-29 20:35:42 +0900

    🌧 :: 채팅 controller 추가

---
+++ src/main/java/com/example/pluck_server/domain/chat/api/ChatController.java
@@ -0,0 +1,32 @@
+package com.example.pluck_server.domain.chat.api;
+
+import com.example.pluck_server.domain.chat.application.ChatService;
+import com.example.pluck_server.domain.chat.domain.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService service;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name){
+        return service.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRooms(){
+        return service.findAllRoom();
+    }
+}
