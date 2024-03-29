b692f591a8b33ea802b4c5c3c89ba31b64e11928
dkflfkd53 <b57062562@gmail.com>
2024-03-29 20:27:16 +0900

    🌧 :: WebSocketChatHandler 추가

---
+++ src/main/java/com/example/pluck_server/infra/soket/WebSocketChatHandler.java
@@ -0,0 +1,38 @@
+package com.example.pluck_server.infra.soket;
+
+import com.example.pluck_server.domain.chat.application.ChatService;
+import com.example.pluck_server.domain.chat.domain.ChatRoom;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper mapper;
+
+    private final ChatService service;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+
+//        TextMessage textMessage = new TextMessage("Welcome Chatting Server");
+//        session.sendMessage(textMessage);
+
+        ChatMessageDto chatMessage = mapper.readValue(payload, ChatMessageDto.class);
+        log.info("session {}", chatMessage.toString());
+
+        ChatRoom room = service.findRoomById(chatMessage.getRoomId());
+        log.info("room {}", room.toString());
+
+        room.handleAction(session, chatMessage, service);
+    }
+}
