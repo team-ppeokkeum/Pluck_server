3a0aee01f19ad63fddc1ff59c945b9c227ccb4e3
dkflfkd53 <b57062562@gmail.com>
2024-04-05 14:09:30 +0900

    ♻️ :: Alarm entity 속성 이름 수정

--- src/main/java/com/example/pluck_server/domain/alarm/domain/Alarm.java
+++ src/main/java/com/example/pluck_server/domain/alarm/domain/Alarm.java
@@ -29,7 +29,7 @@ public class Alarm {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "alarm_id", nullable = false)
+    @Column(name = "id", nullable = false)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
